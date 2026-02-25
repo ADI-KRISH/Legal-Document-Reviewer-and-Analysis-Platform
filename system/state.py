715a2265bca6b66124b83f89a374c73dcef8263d
GS ADITHYA KRISHNA <adikrishactive@gmail.com>
2026-02-26 00:40:26 +0530

    refactor: rewrite SharedState as TypedDict with LangGraph-compatible annotations

--- system/state.py
+++ system/state.py
@@ -1,24 +1,36 @@
-
-
+from typing import TypedDict,add_messages,Dict
+from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+from pydantic import BaseModel
 from typing import Annotated
+from orchestrator import Orchestrator
+from risk_analyser import Risk_Analyser
+from negotiation_agent import Negotiation_Agent
+from summariser import Summariser
+from QnA_Agent import QnA_Agent
+from negotiation_agent import Negotiation_Agent
+from report_generator import Report_Generator
+from research_agent import Research_Agent
+
+class SharedState(TypedDict):
+    document_uploaded : bool
+    file_name : str
+    negotiation_json : Dict[str,any]
+    risk_json : Dict[str,any]
+    clauses_json : Dict[str,any]
+    report : Annotated[str,add_messages]
+    iteration : int
+    plan : Annotated[list[str],add_messages]
+    user_query : Annotated[list[str],add_messages]
+    response : str
+    synthesizer_in : Annotated[list[str],add_messages]
+    synthesizer_out : Annotated[list[str],add_messages]
+    research_json : Dict[str,any]
+    current_agent : str
+    next_agent : str
+    research_output : Annotated[list[str],add_messages]
 
 
-class SharedState:
-    def __init__(self) -> None:
-        self.negotiation_json = None 
-        self.risk_json = None
-        self.clauses_json = None
-        self.report = None
-        self.iteration = 0
-        self.plan = Annotated[List[str]]
-        self.user_query = Annotated[List[str]]
-        self.response = None
-        self.synthesizer_in = None
-        self.synthesizer_out = None
-        self.research_json = None
-        self.current_agent = None
-        self.next_agent = None
-        self.research_output = None
-        
+
+
     
     
