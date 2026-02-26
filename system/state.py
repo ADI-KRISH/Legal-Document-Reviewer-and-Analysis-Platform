fbc66955670dbee0a05e9275dc5c242ea89abc8c
GS ADITHYA KRISHNA <adikrishactive@gmail.com>
2026-02-27 04:17:45 +0530

    Update state.py and backend Database

--- system/state.py
+++ system/state.py
@@ -1,5 +1,4 @@
-from typing import TypedDict,add_messages,Dict
-from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+from typing import TypedDict,Dict
 from pydantic import BaseModel
 from typing import Annotated
 from orchestrator import Orchestrator
@@ -10,6 +9,9 @@ from QnA_Agent import QnA_Agent
 from negotiation_agent import Negotiation_Agent
 from report_generator import Report_Generator
 from research_agent import Research_Agent
+from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+from langgraph.graph import Staegraph,START,END
+DB_URI = "postgresql://postgres:postgres@localhost:5442/postgres?sslmode=disable"
 
 class SharedState(TypedDict):
     document_uploaded : bool
@@ -28,6 +30,22 @@ class SharedState(TypedDict):
     current_agent : str
     next_agent : str
     research_output : Annotated[list[str],add_messages]
+    document_summary : str
+    state_summary : json = None
+    citations : list[str] = None
+    routing : str = None
+graph = StateGraph()
+def orchestrate(state:SharedState) -> SharedState:
+    orchestrator = Orchestrator()
+    state_sumarry = state["state_summary"]
+    doc_summary = state["document_summary"]
+    user_query = state["user_query"]
+    result = orchestrator.run(user_query=user_query,state_summary=state_sumarry,document_summary=doc_summary)
+    state["plan"] = result.content["steps"].strip().lower()
+    return state
+
+with PostgteGraphSaver.from_conn_string(DB_URI) as checkpointer:  
+    pass
 
 
 
